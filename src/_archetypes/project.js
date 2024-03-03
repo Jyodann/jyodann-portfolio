97cddad304731846c7d7d0213061f31873f3347b
Jordan Khong <jordynwinnie@gmail.com>
2024-03-03 11:10:55 +0800

    Modified Projects Page to include grid

--- src/_archetypes/project.js
+++ src/_archetypes/project.js
@@ -1,13 +1,14 @@
-export default function (title) {
+export default function (title, date) {
   const slug = title.replace(/\s+/g, "-").toLowerCase();
-
+  const checked_date = new Date(date)
   return {
-    path: `./content/projects/${slug}.vto`,
+    path: `./content/projects/${checked_date.getFullYear()}/${slug}.vto`,
     content: {
       layout: "post.vto",
       top_img: "",
       title: title,
       content: "Page content",
+      date: date,
     },
   };
 }
