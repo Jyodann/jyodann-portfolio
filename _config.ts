a4d635b205c04cf90b2bdac3fa98acbac83fdb3c
Jordan Khong <jordynwinnie@gmail.com>
2024-03-17 00:08:17 +0800

    Mimified Js + Cleaned up Files

--- _config.ts
+++ _config.ts
@@ -1,23 +1,24 @@
 import lume from "lume/mod.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
-import postcss from "lume/plugins/postcss.ts";
 import inline from "lume/plugins/inline.ts";
-import minifyHTML from "lume/plugins/minify_html.ts";
 import picture from "lume/plugins/picture.ts";
 import transformImages from "lume/plugins/transform_images.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
 import metas from "lume/plugins/metas.ts";
-import nav from "lume/plugins/nav.ts";
 import favicon from "lume/plugins/favicon.ts";
+import readInfo from "lume/plugins/reading_info.ts";
+import terser from "lume/plugins/terser.ts";
+import postcss from "lume/plugins/postcss.ts";
+
 const site = lume(
     {
         src: "./src",
         dest: "./output"
     }
 );
-
+// Inline SVG Assets
 site.use(inline())
-site.use(minifyHTML())
+// Tailwind Implmentation
 site.use(tailwindcss({
     options : {
         darkMode: 'selector',
@@ -31,10 +32,12 @@ site.use(tailwindcss({
     }
 }
 ))
-site.use(postcss());
-site.use(nav(/* Options */));
+site.use(postcss())
+// Resize Images to optimize for loading:
 site.use(picture());
 site.use(transformImages());
+
+// Add Meta Tags
 site.use(metas());
 site.use(codeHighlight());
 site.use(favicon(
@@ -42,4 +45,11 @@ site.use(favicon(
         input: "./assets/img/favicon.png"
     }
 ));
+
+// Reading Length
+site.use(readInfo());
+
+//Minify Js
+site.use(terser(/* Options */));
+
 export default site;
